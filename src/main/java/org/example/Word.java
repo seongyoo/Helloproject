dd472fd223fc8cde9e0afaa2c608457d0c55c1a4
seongyoo <seonlineseon@gmail.com>
2022-09-02 10:23:24 +0900

    create Hello Project

---
+++ src/main/java/org/example/Word.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Word {
+    private String name;
+    private String meaning;
+
+    public Word() {
+    }
+    public Word(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+}
